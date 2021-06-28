4a2cb6fceddf4fe27a5ab7ed9efd175e2247266a
PolonevichA <andrey.polonevich0209@gmail.com>
2021-06-28 10:57:33 +0300

    Реализовал таймер

--- dist/js/script.js
+++ dist/js/script.js
@@ -94,6 +94,7 @@
 /***/ (function(module, exports) {
 
 window.addEventListener('DOMContentLoaded', () => {
+  // TABS 
   const tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items'),
         tabs = tabsParent.querySelectorAll('.tabheader__item');
@@ -134,7 +135,56 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     ;
-  });
+  }); //TIMER
+
+  const endTime = '2021-12-31';
+
+  function getTimeRemaining(endtime) {
+    const total = Date.parse(endtime) - new Date(),
+          days = Math.floor(total / (1000 * 60 * 60 * 24)),
+          hours = Math.floor(total / (1000 * 60 * 60) % 24),
+          minutes = Math.floor(total / (1000 * 60) % 60);
+    seconds = Math.floor(total / 1000 % 60);
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds
+    };
+  }
+
+  function getZero(elem) {
+    if (elem >= 0 && elem < 10) {
+      return `0${elem}`;
+    } else {
+      return elem;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector('#minutes'),
+          seconds = timer.querySelector('#seconds'),
+          interval = setInterval(updateClock, 1000);
+    updateClock();
+
+    function updateClock() {
+      const time = getTimeRemaining(endtime);
+      days.innerHTML = getZero(time.days);
+      hours.innerHTML = getZero(time.hours);
+      minutes.innerHTML = getZero(time.minutes);
+      seconds.innerHTML = getZero(time.seconds);
+
+      if (time.total <= 0) {
+        clearInterval(interval);
+      }
+    }
+  }
+
+  setClock('.timer', endTime);
 });
 
 /***/ })
--- src/js/main.js
+++ src/js/main.js
@@ -37,5 +37,57 @@ window.addEventListener('DOMContentLoaded', () => {
         };
     });
     //TIMER
-    
+    const endTime = '2021-12-31';
+
+    function getTimeRemaining(endtime){
+        const total = Date.parse(endtime) - new Date(),
+              days = Math.floor(total / (1000 * 60 * 60 * 24)),
+              hours = Math.floor(total / (1000 * 60 * 60) % 24),
+              minutes = Math.floor(total / (1000 * 60) % 60)
+              seconds = Math.floor((total / 1000) % 60);
+        
+        return{
+            total,
+            days,
+            hours,
+            minutes,
+            seconds
+        }
+    }
+
+    function getZero(elem){
+        if(elem >= 0 && elem < 10){
+            return `0${elem}`;
+        }else{
+            return elem;
+        }
+    }
+
+    function setClock(selector, endtime){
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              interval = setInterval(updateClock, 1000);
+        
+        updateClock();
+
+        function updateClock(){
+            const time = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(time.days);
+            hours.innerHTML = getZero(time.hours);
+            minutes.innerHTML = getZero(time.minutes);
+            seconds.innerHTML = getZero(time.seconds);
+
+            if(time.total <= 0){
+                clearInterval(interval);
+            }
+        }
+    }
+
+    setClock('.timer', endTime);
+
+
 })
