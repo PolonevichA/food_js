e119800f0de4e6dd12064a6b79e7da93deafef60
PolonevichA <andrey.polonevich0209@gmail.com>
2021-06-30 19:33:14 +0300

    Отправка формы XMLHttpRequest в обычном формате

--- dist/js/script.js
+++ dist/js/script.js
@@ -267,6 +267,43 @@ window.addEventListener('DOMContentLoaded', () => {
   new MenuCard("img/tabs/vegy.jpg", "vegy", 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 6, 27.3, ".menu__field .container").render();
   new MenuCard("img/tabs/elite.jpg", "elite", 'Меню “Премиум”', 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 14, 27.3, ".menu__field .container", "menu__item").render();
   new MenuCard("img/tabs/post.jpg", "post", 'Меню "Постное"', 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков. ', 11, 27.3, ".menu__field .container", "menu__item").render(); // 
+  // FORMS 
+
+  const forms = document.querySelectorAll('form');
+  const message = {
+    loading: 'Загрузка ...',
+    success: 'Спасибо! Скоро мы с вами сяжемся',
+    failure: 'Что-то пошло не так...'
+  };
+  forms.forEach(form => {
+    postData(form);
+  });
+
+  function postData(form) {
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      const statusMessage = document.createElement('div');
+      statusMessage.classList.add('status');
+      statusMessage.textContent = message.loading;
+      form.append(statusMessage);
+      const request = new XMLHttpRequest();
+      request.open('POST', 'server.php'); // request.setRequestHeader('Content-type', 'multipart/form-data');
+
+      const formData = new FormData(form);
+      request.send(formData);
+      request.addEventListener('load', () => {
+        if (request.status === 200) {
+          statusMessage.textContent = message.success;
+          form.reset();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 2000);
+        } else {
+          statusMessage.textContent = message.failure;
+        }
+      });
+    });
+  }
 });
 
 /***/ })
--- src/js/main.js
+++ src/js/main.js
@@ -204,4 +204,50 @@ window.addEventListener('DOMContentLoaded', () => {
         "menu__item"
     ).render();
     // 
+
+    // FORMS 
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка ...',
+        success: 'Спасибо! Скоро мы с вами сяжемся',
+        failure: 'Что-то пошло не так...'
+    }
+
+    forms.forEach(form => {
+        postData(form);
+    })
+
+    function postData(form){
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            
+            // request.setRequestHeader('Content-type', 'multipart/form-data');
+            const formData = new FormData(form);
+
+            request.send(formData);
+            request.addEventListener('load', () => {
+                if(request.status === 200){
+                    statusMessage.textContent = message.success;
+                    form.reset();
+
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    },2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            })
+        })
+    }
 })
+
